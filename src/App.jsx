97bb08c0e67686bbc135632b81e42f33deb4a2f8
Damian Ludef <damiinho@gmail.com>
2024-03-05 18:56:12 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <>ee</>;
+}
+
+export default App;
---
+++ src/contexts/AppContext.jsx
@@ -0,0 +1,34 @@
+import { createContext, useEffect, useState } from "react";
+import PropTypes from "prop-types";
+
+export const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const providerValue = {
+    windowWidth,
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <AppContext.Provider value={providerValue}>{children}</AppContext.Provider>
+  );
+};
+
+AppProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AppProvider;
